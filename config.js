a6354116e9aa5c708e30c5468d1c059f34115cfe
sero-98 <aroni.carbajals@gmail.com>
2021-11-23 19:51:55 -0500

    primer commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'linkedinAuth': {
+    'clientID': '7753wrccj510xm', // your App ID
+    'clientSecret': 'J02uRXPQUb5hQxzo', // your App Secret
+    'callbackURL': 'http://127.0.0.1:3000/auth/linkedin/callback'
+  }
+}
